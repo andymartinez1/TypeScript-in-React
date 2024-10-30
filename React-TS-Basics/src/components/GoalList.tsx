2dd8d0d6e734f9f2d98dfc592116defd5e25d4aa
Andrew Martinez <andy72391@gmail.com>
2024-10-30 06:55:40 -0400

    Added a warning info box to alert the user when they are adding too many goals and when none are set yet.

--- React-TS-Basics/src/components/GoalList.tsx
+++ React-TS-Basics/src/components/GoalList.tsx
@@ -1,5 +1,7 @@
+import { ReactNode } from "react";
 import { type Goal as IGoal } from "../App";
 import Goal from "./Goal";
+import InfoBox from "./InfoBox";
 
 interface GoalListProps {
   goals: IGoal[];
@@ -7,15 +9,28 @@ interface GoalListProps {
 }
 
 export default function GoalList({ goals, onDeleteGoal }: GoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">There are no goals yet.</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = <InfoBox mode="warning">Too many goals</InfoBox>;
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <Goal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </Goal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <Goal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </Goal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
