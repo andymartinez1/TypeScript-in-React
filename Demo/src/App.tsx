5971c17140e3c02d581a12c2131ca5433e1569f4
Andrew Martinez <andy72391@gmail.com>
2024-10-30 21:09:23 -0400

    Beginning a new project using TypeScript and React.

---
+++ Demo/src/App.tsx
@@ -0,0 +1,16 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <main>
+      <p>
+        <Button>A button</Button>
+      </p>
+      <p>
+        <Button href="https://www.google.com">A link</Button>
+      </p>
+    </main>
+  );
+}
+
+export default App;
---
+++ Demo/src/components/Container.tsx
@@ -0,0 +1,11 @@
+import { ElementType } from "react";
+
+type ContainerProps = {
+  as: ElementType;
+};
+
+export default function Container({ as }: ContainerProps) {
+  const Component = as;
+
+  return Component;
+}
