aab7b185cee10bcf5eb5c00d7d987cecf74ee9c7
Andrew Martinez <andy72391@gmail.com>
2024-11-01 01:05:11 -0400

    Added a polymorphic component and a forwardRef function.

--- Demo/src/App.tsx
+++ Demo/src/App.tsx
@@ -1,14 +1,15 @@
+import { useRef } from "react";
 import Button from "./components/Button";
+import Container from "./components/Container";
+import Input from "./components/Input";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <p>
-        <Button>A button</Button>
-      </p>
-      <p>
-        <Button href="https://www.google.com">A link</Button>
-      </p>
+      <Container as={Button}>Click me</Container>
+      <Input label="Test" id="test" ref={input} />
     </main>
   );
 }
