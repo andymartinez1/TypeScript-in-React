548388b954528eab37f96d9eda3804ac8a9579b0
Andrew Martinez <andy72391@gmail.com>
2024-11-01 10:45:52 -0400

    Added a form component using the useImperativeHandle in another wrapper.

--- Demo/src/App.tsx
+++ Demo/src/App.tsx
@@ -1,15 +1,26 @@
 import { useRef } from "react";
 import Button from "./components/Button";
-import Container from "./components/Container";
+import Form, { FormHandle } from "./components/Form";
 import Input from "./components/Input";
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
 
   return (
     <main>
-      <Container as={Button}>Click me</Container>
-      <Input label="Test" id="test" ref={input} />
+      <Form onSave={handleSave} ref={customForm}>
+        <Input label="Name" type="text" id="name" />
+        <Input label="Age" type="number" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
